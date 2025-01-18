e9d8dba7a5cb9de966e26ac6528e73312167ece0
Malik H <malik.h@webdevpro.net>
2025-01-18 23:23:15 +0100

    jour04

--- jour04/10-langage.sql
+++ jour04/10-langage.sql
@@ -80,3 +80,61 @@ UPDATE user SET email = 'nouveau@yahoo.fr' , password = 'titi' WHERE id = 1 ;
 
 -- SELECT => 
 
+
+--- 
+---
+
+/*
+vous voulez créer un site internet de réservation de salle de réunion
+
+Dans votre base de données,  vous allez stocker :
+
+des salles de réunion qui ont les caractéristiques suivantes :
+
+nom
+capacite
+description
+url_img
+dt_creation
+
+
+chaque salle est dans un batiment qui se caractérise par 
+
+nom 
+rue 
+code_postal
+ville 
+
+un batiment peut contenir plusieurs salles de réunion par contre une salle de réunion ne peut être située que dans une seul batiment 
+
+
+il y a 3 batiments 
+
+Richelieu / 75 rue de Paris / 75000 / Paris
+Jacques Prevert / 30 rue des Feuilles / 13000 / Marseille
+Victor Hugo / 44 rue Marius Aufant // 92000 / Levallois 
+
+
+dans le batiment Richelieu il y a 2 salles de réunion 
+
+Rouge / 20 / salle avec Vidéo Projecteur / 'img.jpg' / le 1er février 2024
+Bleue / 10 / salle avec bureau / img.jpg / le 1er mars 2024
+
+dans le batiment Jacques Prevert il y a 3 salles de réunion
+
+Rose / 50 / salle / img.jpg / le 1er octobre 2024
+Mauve / 100 / auditorium / img.jpg / le 1er octobre 2010
+Jaune / 15 / salle de réunion / img.jpg / le 1er janvier 2025
+
+
+---- 
+etape 3
+
+Après avoir vu votre base de données, vous décidez de réaliser des rectifications 
+
+- changer le nom de la salle Jaune à Vert
+- changer le code postal du batiement Victor Hugo par 92300
+- supprimer la salle Rouge 
+*/
+
+
